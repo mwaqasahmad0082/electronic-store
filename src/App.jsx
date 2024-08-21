01dc9bbf715a22b4649691e843b13f2bfad67fd2
Muhammad Waqas <mwaqasahmad0082@gmail.com>
2024-08-21 14:19:09 +0500

    electronic-store

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Product from "./pages/Product";
+import Vesite from "./pages/Vesite";
+import Discount from "./Pages/Discount";
+import Slider from "./pages/Slider";
+import Services from "./pages/Services";
+import Contect from "./pages/Contect";
+import Layout from "./pages/Layout";
+
+function App() {
+  const router = createBrowserRouter([
+   
+    {
+      path: "/About",
+      element: (
+        <Layout>
+          <About />
+        </Layout>
+      ),
+    },
+    {
+      path: "/Product",
+      element: (
+        <Layout>
+          <Product />
+        </Layout>
+      ),
+    },
+    {
+      path: "/Vesite",
+      element: (
+        <Layout>
+          <Vesite />
+        </Layout>
+      ),
+    },
+    {
+      path: "/Discount",
+      element: (
+        <Layout>
+          <Discount />
+        </Layout>
+      ),
+    },
+    {
+      path: "/Slider",
+      element: (
+        <Layout>
+          <Slider />
+        </Layout>
+      ),
+    },
+    {
+      path: "/Services",
+      element: (
+        <Layout>
+          <Services />
+        </Layout>
+      ),
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
